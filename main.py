1a2796e50b3501377e072088f991db2bfbd66789
IgorPono <Igor30673@5892>
2023-09-11 01:29:26 -0500

    Changed main to be able to detect for optimal weight for individual variable

--- main.py
+++ main.py
@@ -16,12 +16,27 @@ import pandas as pd
 correctPredictions = 0
 incorrectPredictions = 0
 totalPredictions = 0
+highestAcc = 0
+highestAccWeight = 0
 # numProcessed = 0
+pClass3Weight = -1
+pClass2Weight = 0
+pClass1Weight = 0
+maleWeight = 0
+femaleWeight = 0
+ageLess10Weight = 0
+age10to20Weight = 0
+age20to30Weight = 0
+age30to40Weight = 0
+age40to50Weight = 0
+age50to60Weight = 0
+age60AboveWeight = 0
 
 def calcSurvival(arr):
-    global totalPredictions
-    global correctPredictions
-    global incorrectPredictions
+    global totalPredictions, correctPredictions, incorrectPredictions, highestAcc
+    global pClas3Weight, pClass2Weight, pClass1Weight
+    global maleWeight, femaleWeight
+    global ageLess10Weight, age20to30Weight, age30to40Weight, age40to50Weight, age50to60Weight,  age60AboveWeight
     totalPredictions += 1
     sum = 0
     for i in range(arr.size):
@@ -31,32 +46,36 @@ def calcSurvival(arr):
         if(i == 2):
             pClass = arr[i]
             if pClass == 3:
-                sum += 0.1
+                sum += pClass3Weight
             elif pClass == 2:
-                sum -= 0.7
+                sum += pClass2Weight
             else:
-                sum -= 0.1
+                sum += pClass1Weight
             #print('this is pClass ' + str(arr[i]))
         if(i == 4):
             sex = arr[i]
             #print('this is sex ' + str(arr[i]))
             if sex == 'male':
-                sum -= 0.2
+                sum += maleWeight
             else:
-                sum += 0.8
+                sum += femaleWeight
         if(i == 5):
             age = arr[i]
             #print('this is age ' + str(age))
             if age > 10:
-                sum += 0.05
+                sum += ageLess10Weight
             elif age > 10 and age < 20:
-                sum += 0.1
+                sum += age10to20Weight
             elif age > 20 and age < 30:
-                sum -= 0.4
+                sum += age20to30Weight
             elif age > 30 and age < 40:
-                sum += 0.5
+                sum += age30to40Weight
+            elif age > 40 and age < 50:
+                sum += age40to50Weight
+            elif age > 50 and age < 60:
+                sum += age50to60Weight
             else:
-                sum -= 0.05
+                sum += age60AboveWeight
     if arr[1] == 1:
         if sum >= 0:
             # correctPredictions+=1
@@ -72,14 +91,37 @@ def calcSurvival(arr):
         else:
             incorrectPredictions += 1
             # print("individual did not survive, prediction says they did")
-    print(correctPredictions/totalPredictions)
-    #print(sum)
+    #print(correctPredictions/totalPredictions)
 
 train = pd.read_csv('train.csv')
-for i in range(len(train)):
+
+for i in range(0, 200):
+    correctPredictions = 0
+    incorrectPredictions = 0
+    totalPredictions = 0
+    for j in range(len(train)):
+        #print(totalPrediction)
+        calcSurvival(train.iloc[j].values)
+        #print(pClass3Weight)
+    #print('about to calculate accuracy')
+    #print(correctPredictions, totalPrediction)
+    #print("weight: " + str(pClass3Weight))
+    #print("acc: " + str(correctPredictions/totalPredictions))
+    if highestAcc < (correctPredictions/totalPredictions):
+        highestAcc = correctPredictions/totalPredictions
+        highestAccWeight = pClass3Weight
+        #print(highestAccWeight)
+        #print(highestAcc)
+    # highestAcc = max(highestAcc, correctPredictions/totalPredictions)
+    pClass3Weight += 0.01
+print()
+print(highestAcc)
+print(highestAccWeight)
+
+#for i in range(len(train)):
 #print(len(train))
 #for i in range(0, 100):
-    calcSurvival(train.iloc[i].values)
+#    calcSurvival(train.iloc[i].values)
     # calcSurvival(train.iloc[i].to_numpy)
     # print(train.iloc[i])
     # print()
